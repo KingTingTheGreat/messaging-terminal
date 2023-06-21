06fc3027d4c0bddde1feaf4c68e69e0c37c3b5fc
KingTingTheGreat <jting@bu.edu>
2023-06-21 01:15:09 -0400

    changing port to 5555 to make it easier on the client side. will probably change this back at some point

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ sock.bind(('0.0.0.0', 0))
 server_port = sock.getsockname()[1]
 sock.close()
 sock:socket.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('0.0.0.0', server_port))
+sock.bind(('0.0.0.0', 5555))
 
 connections = {}
 
